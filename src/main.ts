ddec46ab7a2b2bd00cbcd41b525eafaf6dacdd39
Nicolas Galipot <nicolas.galipot@wolterskluwer.com>
2023-03-08 15:06:39 +0100

    add rudimentary language definition for nsharp

--- src/main.ts
+++ src/main.ts
@@ -16,7 +16,46 @@ monaco.languages.register({
   configuration: configPath
 });
 
+monaco.languages.setMonarchTokensProvider('nsharp', {
+  ignoreCase: true,
+  keywords: ["dim", "class", "public", "private", "if", "case", "for", "foreach", "while", "break", "continue"],
+  builtins: ["debugprint"],
+  tokenizer: {
+    root: [
+      [/[a-z][\w]*#/, 'constant'],
+      [/([+\-*\/\^&:]:=|=|<|<=|>|>=|=>)/, 'operators'],
+      [ /[a-z][\w]*[@$%]?/, {
+        cases: {
+          '@keywords': 'keyword',
+          '@default': 'variable',
+          '@builtins': 'predefined',
+        }
+      } ],
+      [/[0-9]+[.]?[0-9]*/, 'number.float'],
+      [/".*?"/, 'string'],
+      [/'.*?$/, 'comment']
+    ]
+  }
+});
+
 monaco.editor.create(document.querySelector<HTMLDivElement>('#app')!, {
-  value: 'console.log("Hello, world")',
-  language: 'javascript',
+  value: `Dim factorial@ := (n@ Number+) => (
+    Dim fac@ := 1:
+    For(n@,
+        fac@ := fac@ * Inc%
+    ):
+    fac@
+):
+
+Dim range@ := New Array<Number+>():
+For(5,
+    range@.Append(Inc%)
+):
+
+Dim results@ := range@.Select((n@) => (factorial@.Invoke(n@))):
+
+ForEach(results@,
+    DebugPrint("factorial(" & Key% & ") -> " & Val%)
+)`,
+  language: 'nsharp',
 });
