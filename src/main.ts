0b6dbf9bd57fc84d9d7f1c316689096454882077
Nicolas Galipot <nicolas.galipot@wolterskluwer.com>
2023-03-09 12:48:01 +0100

    Fix formating

--- src/main.ts
+++ src/main.ts
@@ -3,143 +3,143 @@ import { monaco } from './mona/customMonaco';
 import init, { javascriptFromBasic, membersForVar } from "../pkg/basic_lang.js";
 
 const langConfig: monaco.languages.LanguageConfiguration = {
-  wordPattern: /(-?\d*\.\d\w*@?#?%?)|([^\`\~\!\^\&\*\(\)\-\=\+\[\{\]\}\\\|\;\:\'\"\,\.\<\>\/\?\s]+)/g,
-  "comments": {
-      // symbol used for single line comment. Remove this entry if your language does not support line comments
-      "lineComment": "'"
-  },
-  // symbols used as brackets
-  "brackets": [
-      ["{", "}"],
-      ["[", "]"],
-      ["(", ")"]
-  ],
-  // symbols that are auto closed when typing
-  "autoClosingPairs": [
-		{ open: '{', close: '}' },
-		{ open: '[', close: ']' },
-		{ open: '(', close: ')' },
-		{ open: '"', close: '"', notIn: ['string'] },
-  ],
-  // symbols that can be used to surround a selection
-  "surroundingPairs": [
-		{ open: '{', close: '}' },
-		{ open: '[', close: ']' },
-		{ open: '(', close: ')' },
-		{ open: '"', close: '"' },
-  ]
+    wordPattern: /(-?\d*\.\d\w*@?#?%?)|([^\`\~\!\^\&\*\(\)\-\=\+\[\{\]\}\\\|\;\:\'\"\,\.\<\>\/\?\s]+)/g,
+    "comments": {
+        // symbol used for single line comment. Remove this entry if your language does not support line comments
+        "lineComment": "'"
+    },
+    // symbols used as brackets
+    "brackets": [
+        ["{", "}"],
+        ["[", "]"],
+        ["(", ")"]
+    ],
+    // symbols that are auto closed when typing
+    "autoClosingPairs": [
+        { open: '{', close: '}' },
+        { open: '[', close: ']' },
+        { open: '(', close: ')' },
+        { open: '"', close: '"', notIn: ['string'] },
+    ],
+    // symbols that can be used to surround a selection
+    "surroundingPairs": [
+        { open: '{', close: '}' },
+        { open: '[', close: ']' },
+        { open: '(', close: ')' },
+        { open: '"', close: '"' },
+    ]
 };
 
-monaco.languages.register({ 
-  id: 'nsharp',
-  aliases: [
-    "NSharp",
-    "nsharp"
-  ],
-  extensions: [
-    "nab"
-  ],
+monaco.languages.register({
+    id: 'nsharp',
+    aliases: [
+        "NSharp",
+        "nsharp"
+    ],
+    extensions: [
+        "nab"
+    ],
 });
 
 monaco.languages.setLanguageConfiguration('nsharp', langConfig);
 
 monaco.languages.setMonarchTokensProvider('nsharp', {
-  ignoreCase: true,
-  keywords: ["dim", "new", "as", "class", "public", "private", "if", "case", "for", "foreach", "while", "break", "continue"],
-  typeKeywords: [
-    'boolean', 'number', 'string', 'dictionary', 'cache', 'form', 'recordset', 'tuple', 'array'
-  ],
-  builtins: ["debugprint"],
-  tokenizer: {
-    root: [
-      [/[a-z][\w]*[#%]/, 'constant'],
-      [/([+\-*\/\^&:]:=|=|<|<=|>|>=|=>)/, 'operators'],
-      [/[a-z][\w]*[@$%]/, 'identifier'],
-      [ /[a-z][\w]*/, {
-        cases: {
-          '@keywords': 'keyword',
-          '@builtins': 'variable.other',
-          '@default': 'variable.other',
-        }
-      } ],
-      [/[0-9]+[.]?[0-9]*/, 'number.float'],
-      [/".*?"/, 'string'],
-      [/'.*?$/, 'comment']
-    ]
-  }
+    ignoreCase: true,
+    keywords: ["dim", "new", "as", "class", "public", "private", "if", "case", "for", "foreach", "while", "break", "continue"],
+    typeKeywords: [
+        'boolean', 'number', 'string', 'dictionary', 'cache', 'form', 'recordset', 'tuple', 'array'
+    ],
+    builtins: ["debugprint"],
+    tokenizer: {
+        root: [
+            [/[a-z][\w]*[#%]/, 'constant'],
+            [/([+\-*\/\^&:]:=|=|<|<=|>|>=|=>)/, 'operators'],
+            [/[a-z][\w]*[@$%]/, 'identifier'],
+            [/[a-z][\w]*/, {
+                cases: {
+                    '@keywords': 'keyword',
+                    '@builtins': 'variable.other',
+                    '@default': 'variable.other',
+                }
+            }],
+            [/[0-9]+[.]?[0-9]*/, 'number.float'],
+            [/".*?"/, 'string'],
+            [/'.*?$/, 'comment']
+        ]
+    }
 });
 
 monaco.languages.registerCompletionItemProvider('nsharp', {
-  triggerCharacters: ['.'],
-  provideCompletionItems(model, position, context, token) {
-    if (context.triggerCharacter !== '.') {
-      return { suggestions: [] };
+    triggerCharacters: ['.'],
+    provideCompletionItems(model, position, context, token) {
+        if (context.triggerCharacter !== '.') {
+            return { suggestions: [] };
+        }
+        const word = model.getWordUntilPosition(position);
+        const prevWord = model.getWordUntilPosition({ lineNumber: position.lineNumber, column: position.column - 1 });
+        const wordArraySuggestions = membersForVar(model.getValue(), prevWord.word);
+
+        const range = {
+            startLineNumber: position.lineNumber,
+            endLineNumber: position.lineNumber,
+            startColumn: word.startColumn,
+            endColumn: word.endColumn
+        }
+        const suggestions: monaco.languages.CompletionItem[] = [];
+
+        wordArraySuggestions.forEach((word: string) => {
+            suggestions.push({
+                label: word,
+                kind: monaco.languages.CompletionItemKind.Method,
+                insertText: word,
+                range: range,
+            });
+        });
+
+        return { suggestions };
     }
-    const word = model.getWordUntilPosition(position);
-    const prevWord = model.getWordUntilPosition({ lineNumber: position.lineNumber, column: position.column - 1 });
-    const wordArraySuggestions = membersForVar(model.getValue(), prevWord.word);
-
-    const range = {
-      startLineNumber: position.lineNumber,
-      endLineNumber: position.lineNumber,
-      startColumn: word.startColumn,
-      endColumn: word.endColumn
-    }
-    const suggestions: monaco.languages.CompletionItem[] = [];
-
-    wordArraySuggestions.forEach((word: string) => {
-      suggestions.push({
-        label: word,
-        kind: monaco.languages.CompletionItemKind.Method,
-        insertText: word,
-        range: range,
-      });
-    });
-
-    return { suggestions };
-  }
 });
 
 const urlParams = new URLSearchParams(window.location.search);
 const value = urlParams.get("code") ?? `Dim factorial@ := (n@ Number+) => (
-  Dim fac@ := 1:
-  For(n@,
-      fac@ := fac@ * Inc%
-  ):
-  fac@
+    Dim fac@ := 1:
+    For(n@,
+        fac@ := fac@ * Inc%
+    ):
+    fac@
 ):
 
 Dim range@ := New Array<Number+>():
 For(5,
-range@.Append(Inc%)
+    range@.Append(Inc%)
 ):
 
 Dim results@ := range@.Select((n@) => (factorial@.Invoke(n@))):
 
 ForEach(results@,
-DebugPrint("factorial(" & Key% & ") -> " & Val%)
+    DebugPrint("factorial(" & Key% & ") -> " & Val%)
 )`;
 
 const editor = monaco.editor.create(document.querySelector<HTMLDivElement>('#editor')!, {
-  wordSeparators: `~!^&*()-=+[{]}\|;:'",.<>/?`,
-  theme: "vs",
-  value,
-  language: 'nsharp',
+    wordSeparators: `~!^&*()-=+[{]}\|;:'",.<>/?`,
+    theme: "vs",
+    value,
+    language: 'nsharp',
 });
 
-function switchCurrentTheme(e:any) {
-  if (e.target.checked != null) {
-    if (e.target.checked) {
-      monaco.editor.setTheme('vs-dark');
-    } else {
-      monaco.editor.setTheme('vs');
+function switchCurrentTheme(e: any) {
+    if (e.target.checked != null) {
+        if (e.target.checked) {
+            monaco.editor.setTheme('vs-dark');
+        } else {
+            monaco.editor.setTheme('vs');
+        }
     }
-  }
 }
 
 const themeSwitch = document.querySelector<HTMLLabelElement>('.switch');
 if (themeSwitch != null) {
-  themeSwitch.addEventListener('click', (e:Event) => switchCurrentTheme(e));
+    themeSwitch.addEventListener('click', (e: Event) => switchCurrentTheme(e));
 }
 
 const out = document.getElementById("out") as HTMLTextAreaElement;
@@ -152,8 +152,8 @@ const closeErrBtn = document.getElementById("close-errors") as HTMLButtonElement
 
 (window as any).$nab = (window as any).$nab ?? {};
 (window as any).$nab.log = (s: string) => {
-  if (out)
-  out.value += s + "\n";
+    if (out)
+        out.value += s + "\n";
 }
 
 function exec() {
@@ -162,13 +162,13 @@ function exec() {
         const built = javascriptFromBasic(input);
         eval(built);
         if (typeof (window as any).$nab.__ans__ !== "undefined") {
-          (window as any).$nab.BuiltIn.debugprint((window as any).$nab.__ans__);
+            (window as any).$nab.BuiltIn.debugprint((window as any).$nab.__ans__);
         }
         errConsole.classList.add("hidden");
     } catch (e) {
         errConsole.classList.remove("hidden");
         if (typeof e === "string") {
-          err.value = e;
+            err.value = e;
         }
     }
 }
